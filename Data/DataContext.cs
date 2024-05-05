c6fffdde5bb8c3aa979cb568d74e43a27dd6605f
efe corukoğlu <efecorukoglu@gmail.com>
2024-05-05 13:06:16 +0300

    ilk migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,9 +5,13 @@ namespace Costdle.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        //public DataContext(DbContextOptions<DataContext> options) : base(options)
+        //{
+
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CostdleDB;Trusted_Connection=true;TrustServerCertificate=true;");
         }
 
         public DbSet<Product> Products { get; set; }
