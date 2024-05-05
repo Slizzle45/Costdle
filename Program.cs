d36ac2523aa7d7354639edc0fb26efe36bfa1092
efe corukoğlu <efecorukoglu@gmail.com>
2024-05-05 13:11:56 +0300

    migration silindi. bugfix

--- Program.cs
+++ Program.cs
@@ -10,10 +10,7 @@ namespace Costdle
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
+            builder.Services.AddDbContext<DataContext>();
 
             app.MapGet("/", () => "Hello World!");
 
